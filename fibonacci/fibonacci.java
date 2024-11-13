ce95d2d3fc93cccdeb0a76871ac73e5579d9cc96
anujchaudhari81 <anujchaudhari232@gmail.com>
2024-11-13 13:46:03 -0500

    tried if loop tp solve 1st 2 sequences and for loop to solve rest, from 3rd" sequence

--- fibonacci/fibonacci.java
+++ fibonacci/fibonacci.java
@@ -1,2 +1,28 @@
+import java.util.Scanner;
+
 public class fibonacci {
+    public static void main(String[] args){
+
+        int k=0,l=1;
+        Scanner scan=new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int ip=scan.nextInt();
+
+        if(ip == 1){
+            System.out.println(k);
+        }else{
+            System.out.print(k+" "+l);
+        }
+
+        for(int i=3; i<= ip;i++){
+            int p=k+l;
+            System.out.print(" "+p);
+            k=l;
+            l=p;
+        }
+
+
+
+
+    }
 }
