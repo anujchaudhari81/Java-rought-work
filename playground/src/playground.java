c9692229bdb682bdb3a1af4ae735104576469ee2
anujchaudhari81 <anujchaudhari232@gmail.com>
2024-11-15 14:14:50 -0500

    tried reversing string in java using recursive method

--- playground/src/playground.java
+++ playground/src/playground.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 
 public class playground {
     public static void main(String[] args) {
-
-    System.out.print("addition is 1+2: "+addi(1,2));
-
+    Scanner scan=new Scanner(System.in);
+    System.out.print("Enter a string:");
+    String str=scan.nextLine();
+    System.out.print("reversed string is "+revString(str));
+    scan.close();
     }
 
 
-    public static int addi(int a,int b) {
-        int c=a
-                int d=b;
-        return c;
+    public static String revString(String str) {
+        if(str.isEmpty()||str.length()==1){
+            return str;
+        }
+
+        return str.charAt(str.length()-1)+revString(str.substring(0,str.length()-1));
+
     }
 }
 
