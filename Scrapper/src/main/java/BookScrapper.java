76398ee95efcff6d1a9cfca6e43c9818214ba652
anujchaudhari81 <anujchaudhari232@gmail.com>
2025-01-13 14:42:39 -0500

    fixed some dependencies isses

--- Scrapper/src/main/java/BookScrapper.java
+++ Scrapper/src/main/java/BookScrapper.java
@@ -1,5 +1,25 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+import javax.lang.model.util.Elements;
+import javax.swing.text.Document;
+import java.io.IOException;
+
+
 public class BookScrapper {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         String url="https://books.toscrape.com";
+
+        try{
+            Document document = (Document) Jsoup.connect(url).get();
+            Elements books = document.select(".product_pod");
+
+            for(Elements bk : books){
+
+            }
+        }catch(IOException  e){
+            e.printStackTrace();
+        }
+
     }
 }
