87018b3d43243e3ae331cd4f268a43c3fd75e9bc
anujchaudhari81 <anujchaudhari232@gmail.com>
2025-01-18 23:34:14 -0500

    added scanner to get URLs from users

--- Scrapper/src/main/java/BookScrapper.java
+++ Scrapper/src/main/java/BookScrapper.java
@@ -3,14 +3,22 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class BookScrapper {
     public static void main(String[] args) {
-        String url = "https://books.toscrape.com";
+
+        String url;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the url: ");
+        url = sc.nextLine();
+        //String url = "https://books.toscrape.com";
 
         try {
-            // Fetch and parse the HTML from the URL
+
+
             Document document = Jsoup.connect(url).get();
+
             // Select all elements with the class 'product_pod'
             Elements books = document.select(".product_pod");
 
