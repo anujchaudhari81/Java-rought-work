8f2f093b430df352aca28c398592e86a6c98ef71
anujchaudhari81 <anujchaudhari232@gmail.com>
2025-01-19 14:55:48 -0500

    i'll try again

--- BookScrapperPro/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.bookscrapperpro {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.bookscrapperpro to javafx.fxml;
-    exports org.example.bookscrapperpro;
-}
