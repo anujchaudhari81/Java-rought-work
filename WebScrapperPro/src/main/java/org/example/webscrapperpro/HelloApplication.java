9b9db4df61b94bbd15132d775027ca1f19a06c24
anujchaudhari81 <anujchaudhari232@gmail.com>
2025-01-25 14:40:17 -0500

    seperated styling stuff and node stuff. all the styling will be done in fxml file only

--- WebScrapperPro/src/main/java/org/example/webscrapperpro/HelloApplication.java
+++ WebScrapperPro/src/main/java/org/example/webscrapperpro/HelloApplication.java
@@ -3,28 +3,13 @@ package org.example.webscrapperpro;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-
-        // Load the FXML file
+    public void start(Stage stage) throws Exception {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-
-        // Set up a BorderPane as the root and add the FXML content to it
-        BorderPane pane = new BorderPane();
-        pane.setStyle("-fx-background-color: #3e4a52;");
-        pane.setCenter(fxmlLoader.load());
-
-        // Create a scene with the BorderPane as the root
-        Scene scene = new Scene(pane, 820, 640);
-
-        // Set the stage properties
+        Scene scene = new Scene(fxmlLoader.load(), 820, 640);
         stage.setTitle("Web Scrapper+");
         stage.setScene(scene);
         stage.show();
