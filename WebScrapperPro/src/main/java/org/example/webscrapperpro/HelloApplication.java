040b96bb13a434f85a9262b42be186656e9477d4
anujchaudhari81 <anujchaudhari232@gmail.com>
2025-01-25 13:13:48 -0500

    added background color, and changed titile

--- WebScrapperPro/src/main/java/org/example/webscrapperpro/HelloApplication.java
+++ WebScrapperPro/src/main/java/org/example/webscrapperpro/HelloApplication.java
@@ -3,6 +3,8 @@ package org.example.webscrapperpro;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +12,20 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+        // Load the FXML file
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+
+        // Set up a BorderPane as the root and add the FXML content to it
+        BorderPane pane = new BorderPane();
+        pane.setStyle("-fx-background-color: #3e4a52;");
+        pane.setCenter(fxmlLoader.load());
+
+        // Create a scene with the BorderPane as the root
+        Scene scene = new Scene(pane, 820, 640);
+
+        // Set the stage properties
+        stage.setTitle("Web Scrapper+");
         stage.setScene(scene);
         stage.show();
     }
